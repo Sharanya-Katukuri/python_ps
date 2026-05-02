12585846ddf3519daa3501314981b6217a766d65
Sharanya-katukuri <katukurisharanya@gmail.com>
2026-05-02 11:09:23 +0530

    qrcode

---
+++ qr.py
@@ -0,0 +1,6 @@
+import qrcode
+# img=qrcode.make("https://tse3.mm.bing.net/th/id/OIP.ieA6hkVKy9dnm7sTI0yT4gHaLH?pid=ImgDet&w=184&h=276&c=7&dpr=1.3&o=7&rm=3")
+# img=qrcode.make("https://drive.google.com/file/d/1nMKBJdnKmn-10IFRQuhsIdb1E-HmOfVW/view?usp=sharing")
+# img=qrcode.make("https://drive.google.com/file/d/14je7eYnI9486gQe17Fm3ysuJ8fEg4hm4/view?usp=sharing")
+img=qrcode.make("https://drive.google.com/file/d/15BN0u9SA4vJ1mJzqfeRB2LGMqrbdxYN5/view?usp=sharing")
+img.show()
